f2820068f974c9645fd2133f3c32415ca7a7eff8
pstadler1990 <pstadler90@gmx.de>
2020-03-16 20:23:48 +0100

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="sass">
+  @import "~purecss-sass"
+
+  #app
+    padding: 1em
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pure-g">
+        <button class="pure-button pure-button-primary">Click me</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
